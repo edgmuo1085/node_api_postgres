29ddc8f907f41660bacad2428063e7e9fe5de3bc
edgmuo1085 <edgmuo1085@gmail.com>
2019-08-14 12:20:35 -0500

    Eliminacion de espacios

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes');
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api/', api);
